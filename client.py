2338771d6b701f631163c121149bff0b1c7847fb
dant40 <32370199+dant40@users.noreply.github.com>
2019-12-04 21:08:02 -0500

    Update client.py

--- client.py
+++ client.py
@@ -2,8 +2,8 @@ from socket import *
 from time import ctime
 
 def client():
-	#127.0.0.1 port 9092
+	#127.0.0.1 port 9090
 	csocket = socket()
-	csocket.connect(("127.0.0.1", 9090))
+	csocket.connect(("127.0.0.1", 9091))
 
 client()
